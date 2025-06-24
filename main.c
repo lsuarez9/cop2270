d882edf6fa0a65cf5754ae8b596f650e2ead9312
lsuarez9 <lsuarez9@mdc.edu>
2025-06-24 18:26:22 -0400

    Create main.c
    This is the entry point for the C program

---
+++ main.c
@@ -0,0 +1,11 @@
+/******************************************************************************
+Welcome to MDC COP2270 C Programming Course.
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to COP2270 C Programming Course");
+
+    return 0;
+}
