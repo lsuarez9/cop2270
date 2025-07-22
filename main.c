a7792aeb6dec83168ed977e0c0ccec2f78ad4773
Fidel De La O <fidel.delao92@gmail.com>
2025-07-21 21:58:49 -0400

    This change includes the addition of the Class Purpose function

--- main.c
+++ main.c
@@ -3,9 +3,20 @@
 *******************************************************************************/
 #include <stdio.h>
 
+void ClassPurpose();
+
 int main()
 {
     printf("Welcome to COP2270 C Programming Course 2024/2025");
 
+    ClassPurpose();
+
     return 0;
+}
+
+void ClassPurpose()
+{
+
+    printf("\n\nThe purpose of this course is to teach the basics of programming in C.\n\n");
+
 }
